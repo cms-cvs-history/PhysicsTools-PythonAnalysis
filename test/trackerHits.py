5feab23833f28756a5fde2bae6e28c42d0c7bd34
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2006-03-22 13:49:03 +0000

    change to new startup and tab completion for not loaded libs use of new iterator generator

--- test/trackerHits.py
+++ test/trackerHits.py
@@ -1,31 +1,26 @@
-# first load the ROOT classes
-# importing all is the safe way
+# first load cmstools and ROOT classes
+from cmstools import *
 from ROOT import *
-
-# the CMS specific rootlogon.C part
-gSystem.Load("libPhysicsToolsFWLite")
-AutoLibraryLoader.enable()
 
 # opening file and accessing branches
 print "Opening SimHit file"
 file = TFile("simevent.root")
 events = file.Get("Events")
 branch = events.GetBranch("PSimHit_r_TrackerHitsTIBLowTof.obj")
+
 simHit = std.vector(PSimHit)()
 branch.SetAddress(simHit)
 
-histo = TH1F("nofhits", "Tof of hits", 100, -0.5, 50)
+histo = TH1F("tofhits", "Tof of hits", 100, -0.5, 50)
 
-# looping over all events 
-nOfEvents = events.GetEntries()
-for ev in range(nOfEvents):
+# loop over all events 
+for ev in all(events):
     branch.GetEntry(ev)
-    size = simHit.size()
-    for i in range(size):
-        histo.Fill(simHit[i].timeOfFlight())
+    for hit in all(simHit):
+        histo.Fill(hit.timeOfFlight())
 
 hFile = TFile("histo.root", "RECREATE")
-histo.Write();
+histo.Write()
 
 gROOT.SetBatch()
 gROOT.SetStyle("Plain")
